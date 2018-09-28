eb7261d1e514350401cadbaf18ab3e5747a1113a
Cody Adcock <codyadcock10@gmail.com>
2018-09-28 11:29:07 -0600

    first Commit

---
+++ ContactsCKAssessment/View Controller/ContactListTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ContactListTableViewController.swift
+//  ContactsCKAssessment
+//
+//  Created by Cody on 9/28/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class ContactListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return ContactController.shared.contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        let contact = ContactController.shared.contacts[indexPath.row]
+        cell.textLabel?.text = contact.name
+        return cell
+    }
+    
+    //    // Override to support editing the table view.
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            // Delete the row from the data source
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //        } else if editingStyle == .insert {
+    //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    //        }
+    //    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetail"{
+            let destinationVC = segue.destination as? ContactDetailViewController
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let contact = ContactController.shared.contacts[indexPath.row]
+            destinationVC?.contact = contact
+        }
+    }
+    
+}
